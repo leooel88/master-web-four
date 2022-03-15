288a11835474deae93b2af3a40f5dee6763d5ed4
trithuan <trithuan91@gmail.com>
2022-03-15 13:48:27 +0100

    controller packaging importation

--- Etape_1/web_quest_java/src/main/java/com/quest/web_quest/WebQuestApplication.java
+++ Etape_1/web_quest_java/src/main/java/com/quest/web_quest/WebQuestApplication.java
@@ -2,8 +2,11 @@ package com.quest.web_quest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.quest"})
 public class WebQuestApplication {
 
 	public static void main(String[] args) {
