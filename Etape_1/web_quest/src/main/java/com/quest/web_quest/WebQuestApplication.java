ce5d97aea479b5d956c9aabc79bbd82f21302052
trithuan <trithuan91@gmail.com>
2022-03-15 12:25:08 +0100

    init

---
+++ Etape_1/web_quest/src/main/java/com/quest/web_quest/WebQuestApplication.java
@@ -0,0 +1,13 @@
+package com.quest.web_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQuestApplication.class, args);
+	}
+
+}
---
+++ Etape_1/web_quest/src/test/java/com/quest/web_quest/WebQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest.web_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
