64a9842b6261f4e82ab1961a5ea8e42f17a417a3
trithuan <trithuan91@gmail.com>
2022-05-13 02:50:08 +0200

    register

--- Projet_libre/buybricks-front/src/pages/Register.js
+++ Projet_libre/buybricks-front/src/pages/Register.js
@@ -23,26 +23,32 @@ const Register = () => {
 
 	return (
 		<div class='flex flex-col grid place-items-center h-screen'>
-		<form onSubmit={handleRegister}>
-			<label htmlFor="username">User name</label>
+		<form onSubmit={handleRegister} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+			<div class="mb-4">
+			<label htmlFor="username" class="block text-gray-700 text-sm font-bold mb-2">User name</label>
 			<input
+				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 				type="text"
 				id="username"
 				name="username"
 				placeholder="Your name"
 				required
 			/>
-
-			<label htmlFor="password">Password</label>
+			</div>
+			<div class="mb-6">
+			<label htmlFor="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
 			<input
+				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 				type="password"
 				id="password"
 				name="password"
 				placeholder="Enter your password"
 				required
 			/>
-
-			<input type="submit" value="Register" />
+			</div>
+			<div class="flex items-center justify-between">
+			<input type="submit" value="Register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"/>
+			</div>
 		</form>
 		</div>
 	);
